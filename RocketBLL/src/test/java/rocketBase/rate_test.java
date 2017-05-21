cde697492138cb2b54eb1ea43c8128503d2d16ad
ryan <ryan@128.175.165.188>
2017-05-21 00:06:31 -0400

    update test

--- RocketBLL/src/test/java/rocketBase/rate_test.java
+++ RocketBLL/src/test/java/rocketBase/rate_test.java
@@ -9,12 +9,6 @@ import exceptions.RateException;
 
 public class rate_test {
 
-	//TODO - RocketBLL rate_test
-	//		Check to see if a known credit score returns a known interest rate
-	
-	//TODO - RocketBLL rate_test
-	//		Check to see if a RateException is thrown if there are no rates for a given
-	//		credit score
 	@AfterClass
 	public static void afterstuff() throws RateException{
 		System.out.println(RateBLL.getRate(625));
